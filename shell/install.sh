96883d8210073d566d3e104daea530b7fb228c24
Lauri P. Laux Jr <lauripaulo@gmail.com>
2018-03-10 22:33:06 -0300

    Quick install. It copies the .sh files to the home directory and make them executables.

---
+++ shell/install.sh
@@ -0,0 +1,12 @@
+cp pmap-observer.sh ~/
+cp tracetool.sh ~/
+cp tracetoolsimulator.sh ~/
+cp tracetoolskip.sh ~/
+cp valgrindexec.sh ~/
+
+chmod +x ~/pmap-observer.sh
+chmod +x ~/tracetool.sh
+chmod +x ~/tracetoolsimulator.sh
+chmod +x ~/tracetoolskip.sh
+chmod +x ~/valgrindexec.sh
+
