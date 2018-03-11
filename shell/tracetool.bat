32edb9914ec7d3512fe2420690260ab4e20de31d
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-03-10 21:54:16 -0300

    Improve BAT files readability, more flexible to change.

--- shell/tracetool.bat
+++ shell/tracetool.bat
@@ -1,1 +1,5 @@
-java -Djava.io.tmpdir=C:\Temp -jar D:\Junior\git\ufpr\jBluePill\jbluepill\target\jbluepill-0.9.2-jar-with-dependencies.jar br.ufpr.dinf.arch.jbluepill.TraceToolSimulator --mode:socket --outputfile:tracefile --max:5000000 --xmldata:execution.xml --compress
+@SET TEMP_FOLDER = C:\Temp
+@SET JAR_FOLDER = D:\Junior\git\tracetool\java\target
+@SET JAR = tracetool-all-0.9.2-jar-with-dependencies.jar
+
+java -Djava.io.tmpdir=%TEMP_FOLDER% -jar %JAR_FOLDER%\%JAR% br.ufpr.dinf.arch.jbluepill.TraceToolSimulator --mode:socket --outputfile:tracefile --max:5000000 --xmldata:execution.xml --compress
--- shell/tracetoolsimulatorlimit.bat
+++ shell/tracetoolsimulatorlimit.bat
@@ -1,1 +1,5 @@
-java -Djava.io.tmpdir=C:\Temp -cp D:\Junior\git\ufpr\jBluePill\jbluepill\target\jbluepill-0.9.2-jar-with-dependencies.jar br.ufpr.dinf.arch.jbluepill.TraceToolSimulatorLimit --xmlfile:execution-win.xml --memmap:pmap-final.txt --csvexport:memtable.csv --results:simulation-results.txt
+@SET TEMP_FOLDER = C:\Temp
+@SET JAR_FOLDER = D:\Junior\git\tracetool\java\target
+@SET JAR = tracetool-all-0.9.2-jar-with-dependencies.jar
+
+java -Djava.io.tmpdir=%TEMP_FOLDER% -cp %JAR_FOLDER%\%JAR% br.ufpr.dinf.arch.jbluepill.TraceToolSimulatorLimit --xmlfile:execution-win.xml --memmap:pmap-final.txt --csvexport:memtable.csv --results:simulation-results.txt
